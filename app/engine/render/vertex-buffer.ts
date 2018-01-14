1c1db41ad15105ba21c99f56a4f82d51bec176b3
perfectdaemon <perfectdaemon@gmail.com>
2018-01-14 19:16:49 +0700

    added vertex buffer implementation

--- app/engine/render/vertex-buffer.ts
+++ app/engine/render/vertex-buffer.ts
@@ -1,50 +1,77 @@
-import { VertexFormat, VertexBufferId, VertexBufferUsage, VertexBufferMapAccess } from "./webgl-types";
+import { VertexFormat, VertexBufferId, VertexBufferUsage, VertexBufferMapAccess, VertexAtrib } from "./webgl-types";
 import { gl } from "./webgl";
 
+/**
+ * Вершинный буфер
+ */
 export class VertexBuffer {
-  private buffer: WebGLBuffer;
+  public buffer: WebGLBuffer;
 
-  private _vertexBufferUsage = [
-    {
-      key: VertexBufferUsage.StaticDraw,
-      value: gl.STATIC_DRAW,
-    },
-  ];
+  constructor(public format: VertexFormat) {
 
-  constructor(
-    data: number[],
-    public count: number,
-    public format: VertexFormat,
-    public usage: VertexBufferUsage) {
-    
-      this.buffer = <WebGLBuffer>gl.createBuffer();
+    this.buffer = <WebGLBuffer>gl.createBuffer();
     if (!this.buffer) {
       console.log('Error while creating Vertex Buffer');
       return;
     }
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), usage);
+    this.bind();
+    gl.bufferData(gl.ARRAY_BUFFER, 65535, gl.STATIC_DRAW);
   }
 
-
   public bind(): void {
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.buffer);
 
+    switch (this.format) {
+      case VertexFormat.Pos3Tex2:
+        gl.enableVertexAttribArray(VertexAtrib.vaCoord);
+        gl.enableVertexAttribArray(VertexAtrib.vaTexCoord0);
+        gl.vertexAttribPointer(VertexAtrib.vaCoord, 3, gl.FLOAT, false, 20, 0);
+        gl.vertexAttribPointer(VertexAtrib.vaTexCoord0, 2, gl.FLOAT, false, 20, 12);
+        break;
+
+      case VertexFormat.Pos2Tex2:
+        gl.enableVertexAttribArray(VertexAtrib.vaCoord);
+        gl.enableVertexAttribArray(VertexAtrib.vaTexCoord0);
+        gl.vertexAttribPointer(VertexAtrib.vaCoord, 2, gl.FLOAT, false, 16, 0);
+        gl.vertexAttribPointer(VertexAtrib.vaTexCoord0, 2, gl.FLOAT, false, 16, 8);
+        break;
+
+      case VertexFormat.Pos3Tex2Nor3:
+        gl.enableVertexAttribArray(VertexAtrib.vaCoord);
+        gl.enableVertexAttribArray(VertexAtrib.vaTexCoord0);
+        gl.enableVertexAttribArray(VertexAtrib.vaNormal);
+        gl.vertexAttribPointer(VertexAtrib.vaCoord, 3, gl.FLOAT, false, 32, 0);
+        gl.vertexAttribPointer(VertexAtrib.vaTexCoord0, 2, gl.FLOAT, false, 32, 12);
+        gl.vertexAttribPointer(VertexAtrib.vaNormal, 3, gl.FLOAT, false, 32, 20);
+        break;
+
+      case VertexFormat.Pos3Tex2Col4:
+        gl.enableVertexAttribArray(VertexAtrib.vaCoord);
+        gl.enableVertexAttribArray(VertexAtrib.vaTexCoord0);
+        gl.enableVertexAttribArray(VertexAtrib.vaColor);
+        gl.vertexAttribPointer(VertexAtrib.vaCoord, 3, gl.FLOAT, false, 36, 0);
+        gl.vertexAttribPointer(VertexAtrib.vaTexCoord0, 2, gl.FLOAT, false, 36, 12);
+        gl.vertexAttribPointer(VertexAtrib.vaColor, 4, gl.FLOAT, false, 36, 20);
+        break;
+    }
   }
 
   public free(): void {
     gl.deleteBuffer(this.buffer);
   }
 
-  public update(data: any, start: number, count: number): void {
-
+  public update(data: any, start: number): void {
+    this.bind();
+    gl.bufferSubData(gl.ARRAY_BUFFER, start, data);
   }
-
+/*
   public map(access: VertexBufferMapAccess = VertexBufferMapAccess.ReadWrite): any {
-
+    console.log('VertexBuffer.map is not implemented');
+    return null;
   }
 
   public unmap(): void {
-
-  }
+    console.log('VertexBuffer.unmap is not implemented');
+  }*/
 }
