83c0c7ed23c60e8fd00f01b3a74ffab449130e28
perfectdaemon <perfectdaemon@gmail.com>
2018-01-17 00:11:03 +0700

    finished ShaderProgram implementation

--- app/engine/render/shader-program.ts
+++ app/engine/render/shader-program.ts
@@ -1,15 +1,16 @@
 import { VertexAtrib, ShaderProgramId, ShaderId } from "./webgl-types";
-import { gl } from "./webgl";
+import { gl, renderer } from "./webgl";
 
 export enum ShaderType { Vertex, Fragment };
 export enum UniformType { Vec1, Vec2, Vec3, Vec4, Mat4, Sampler, Int };
 
 export class UniformInfo {
-  type: UniformType;
-  name: string;
-  count: number;
-  index: number;
-  data: any;
+  constructor(
+    public type: UniformType,
+    public name: string,
+    public count: number,
+    public index: WebGLUniformLocation,
+    public data: any) { }
 }
 
 export class ShaderProgram {
@@ -23,7 +24,7 @@ export class ShaderProgram {
   constructor() {
     this.program = <WebGLProgram>gl.createProgram();
     if (!this.program) {
-      console.log('Shader Program create failed');
+      console.log('ShaderProgram create failed');
       return;
     }
   }
@@ -55,7 +56,7 @@ export class ShaderProgram {
     const shader = <WebGLShader>gl.createShader(glShaderType);
 
     if (!shader) {
-      console.log(`ShaderProgram.attach failed - can't create shader with type ${glShaderType} (${shaderType})`);
+      console.log(`ShaderProgram.attach() failed - can't create shader with type ${glShaderType} (${shaderType})`);
       return;
     }
 
@@ -64,7 +65,7 @@ export class ShaderProgram {
     gl.shaderSource(shader, source);
     gl.compileShader(shader);
     if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-      console.log(`ShaderProgram.attach failed - can't compile shader
+      console.log(`ShaderProgram.attach() failed - can't compile shader
        InfoLog:`);
       const infoLog = gl.getShaderInfoLog(shader);
       console.log(infoLog);
@@ -84,13 +85,42 @@ export class ShaderProgram {
 
   public link(): void {
 
+    // Link
+    gl.linkProgram(this.program);
+    if (!gl.getProgramParameter(this.program, gl.LINK_STATUS)) {
+      console.log(`ShaderProgram.link() failed at link
+        InfoLog:`);
+      const infoLog = gl.getProgramInfoLog(this.program);
+      console.log(infoLog);
+    }
+
+    //Validate
+    gl.validateProgram(this.program);
+    if (!gl.getProgramParameter(this.program, gl.VALIDATE_STATUS)) {
+      console.log(`ShaderProgram.link() failed at validate
+        InfoLog:`);
+      const infoLog = gl.getProgramInfoLog(this.program);
+      console.log(infoLog);
+    }
+
+    //Set shared uniforms
+    this.addUniform(UniformType.Mat4, 1, 'uModelViewProj', renderer.renderParams.modelViewProjection);
+    this.addUniform(UniformType.Vec4, 1, 'uColor', renderer.renderParams.color);
   }
 
-  public addUniform(uniformType: UniformType, count: number, name: string, data: any = null): number {
-    return 0;
+  public addUniform(uniformType: UniformType, count: number, name: string, data: any = null): number | null {
+    const index = gl.getUniformLocation(this.program, name);
+
+    if (!index) {
+      console.log(`ShaderProgram.addUniform failed - gl.getUniformLocation for ${name} returned null (not found)`);
+      return null;
+    }
+
+    let uniformInfo = new UniformInfo(uniformType, name, count, index, data);
+    return this.uniforms.push(uniformInfo);
   }
-  public getUniformIndexByName(name: string): number {
-    return 0;
+  public getUniformIndexByName(name: string): WebGLUniformLocation | null {
+    return this.uniforms.filter(uniform => uniform.name === name)[0].index || null;
   }
   /*
   public setUniform(uniformType: UniformType, count: number, value: any, name: string, index: number = -1): void {
@@ -98,7 +128,19 @@ export class ShaderProgram {
   }*/
 
   public setUniform(internalIndex: number, value: any): void {
+    let uniform = this.uniforms[internalIndex];
 
+    uniform.data = value;
+
+    switch (uniform.type) {
+      case UniformType.Vec1: gl.uniform1fv(uniform.index, value); break;
+      case UniformType.Vec2: gl.uniform2fv(uniform.index, value); break;
+      case UniformType.Vec3: gl.uniform3fv(uniform.index, value); break;
+      case UniformType.Vec4: gl.uniform4fv(uniform.index, value); break;
+      case UniformType.Mat4: gl.uniformMatrix4fv(uniform.index, false, value); break;
+      case UniformType.Sampler: gl.uniform1iv(uniform.index, value); break;
+      case UniformType.Int: gl.uniform1iv(uniform.index, value); break;
+    }
   }
 
   private getWebGLShaderType(shaderType: ShaderType): number {
