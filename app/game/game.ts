5747d9d57c07044d8c04389368a158477a3ed0dc
perfectdaemon <perfectdaemon@gmail.com>
2018-01-17 23:18:08 +0700

    Trying to display colored triangle. Having a problem with nonserial gl.bindAttribLocation(0, 2, 4) fails. Need to go deeper

--- app/game/game.ts
+++ app/game/game.ts
@@ -1,40 +1,85 @@
-import { Player } from './player'
 import { Vector2 } from './../engine/math/vector';
 import { GameBase } from './../engine/game-base';
+import { VertexBuffer } from '../engine/render/vertex-buffer';
+import { IndexBuffer } from '../engine/render/index-buffer';
+import { ShaderProgram, ShaderType } from '../engine/render/shader-program';
+import { VertexFormat, IndexFormat } from '../engine/render/webgl-types';
 
 /**
  * Main game class with game loop
  */
 export class Game extends GameBase {
 
-    private player: Player;
+  private vertexData = [
+    0.0,  0.5, 0.0, 1.0, 1.0,   1.0, 0.0, 0.0, 1.0,
+   -0.5, -0.5, 0.0, 1.0, 1.0,   0.0, 1.0, 0.0, 1.0,
+    0.5, -0.5, 0.0, 1.0, 1.0,   0.0, 0.0, 1.0, 1.0
+  ];
 
-    constructor (protected canvasElement: HTMLCanvasElement) {
-        super(canvasElement);
-    }
+  private vertexShader = `
+  attribute vec3 vaCoord;
+  attribute vec2 vaTexCoord0;
+  attribute vec4 vaColor;
 
-    protected onInit(): void {
-        console.log('Run, TypeScript, run!');
-        this.player = new Player();
-        this.player.position = new Vector2(400, 300);
-        this.renderer.setClearColorRGB(0.0, 0.2, 1.0, 0.5);
-    }
+  varying vec4 vColor;
+  varying vec2 texCoord0;
 
-    protected onUpdate(timestamp: number): void {
+  void main(void) {
+    texCoord0 = vaTexCoord0;
+    vColor = vaColor;
+    gl_Position = vec4(vaCoord, 1.0);
+  }
+  `;
 
-    }
+  private fragmentShader = `
+  varying lowp vec2 texCoord0;
+  varying lowp vec4 vColor;
 
-    protected onRender(timestamp: number): void {
-        super.onRender(timestamp);
-        /*this.renderer.drawText(`Hello, world: ${timestamp}`, new Vector2(5, 10));
-        this.renderer.drawText(`player position: ${this.player.position}`, new Vector2(5, 20));
-        this.renderer.drawText(`player look: ${this.player.look}`, new Vector2(5, 30));
-        this.renderer.drawText(`player look angle: ${this.player.look.toAngle()}`, new Vector2(5, 40));
-        */
-        // this.player.render(this.renderer);
-    }
+  void main(void)
+  {
+    gl_FragColor = vColor;
+  }
+  `;
 
-    protected onMouseMove(position: Vector2): void {
-        this.player.look = position.subtract(this.player.position);
-    }
+  private _vb: VertexBuffer;
+  private _ib: IndexBuffer;
+  private _shader: ShaderProgram;
+
+  constructor(protected canvasElement: HTMLCanvasElement) {
+    super(canvasElement);
+
+    this._vb = new VertexBuffer(VertexFormat.Pos3Tex2Col4, 3);
+    this._vb.update(this.vertexData, 0);
+
+    this._ib = new IndexBuffer(IndexFormat.Byte, 3);
+    this._ib.update([0, 1, 2], 0);
+
+
+    this._shader = new ShaderProgram();
+    this._shader.attach(ShaderType.Vertex, this.vertexShader);
+    this._shader.attach(ShaderType.Fragment, this.fragmentShader);
+    this._shader.link();
+  }
+
+  protected onInit(): void {
+    console.log('Run, TypeScript, run!');
+    this.renderer.setClearColorRGB(0.0, 0.2, 1.0, 0.5);
+
+
+  }
+
+  protected onUpdate(timestamp: number): void {
+
+  }
+
+  protected onRender(timestamp: number): void {
+    super.onRender(timestamp);
+    this._vb.bind();
+    this._shader.bind();
+    this.renderer.drawTriangles(this._vb, this._ib, 0, 3);
+  }
+
+  protected onMouseMove(position: Vector2): void {
+
+  }
 }
