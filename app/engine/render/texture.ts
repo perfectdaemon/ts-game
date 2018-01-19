8d9312088f7383f6b8f4b246d1489ff0580ed50d
Dmitry Orlov <perfectdaemon@gmail.com>
2018-01-19 19:20:51 +0700

    #1 added method signatures to Texture class (no implementation)

--- app/engine/render/texture.ts
+++ app/engine/render/texture.ts
@@ -1,3 +1,40 @@
-export class Texture {
+import { TextureFormat, TextureWrap, TextureFilter } from "./webgl-types";
 
+export enum TextureExt { bmp, tga }
+
+
+export class Texture {
+  constructor (data: any,
+    public width: number,
+    public height: number,
+    public format: TextureFormat) {
+
+    }
+
+  public free(): void {
+
+  }
+
+  public setWrapS(wrap: TextureWrap): void {
+
+  }
+
+  public setWrapT(wrap: TextureWrap): void {
+
+  }
+
+  public setWrapR(wrap: TextureWrap): void {
+
+  }
+
+  public setFilter(filter: TextureFilter): void {
+
+  }
+
+  public bind(sampler: number = 0): void {
+
+  }
+  public static unbind(): void {
+
+  }
 }
