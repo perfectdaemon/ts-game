f96c1499a1b762a60637f01142dcc37d7bd72185
perfectdaemon <perfectdaemon@gmail.com>
2018-02-24 00:09:13 +0700

    player class refactoring

--- app/game/garbage-game/player.ts
+++ app/game/garbage-game/player.ts
@@ -18,6 +18,7 @@ export class Player {
   speed: number = defaultSpeed;
   body: Sprite = new Sprite(frameSize * 2, frameSize * 2);
   weapon: Sprite = new Sprite();
+
   collider: Circle = new Circle(
     new Vector2(this.body.position.x, this.body.position.y),
     this.body.width / 2 - colliderReduceSize);
@@ -27,18 +28,23 @@ export class Player {
 
   private currentAnimationYCoord: number = 0;
   private currentAnimationXCoord: number = 0;
-  private moveVector: Vector2 = new Vector2(0, 0);
-  private weaponPosition: Vector2 = new Vector2(0, 0);
   private animationTimer: number = 0;
-  private flippedX: boolean = false;
-  private weaponUpCorrection: number = 0;
-  private weaponZ: number = 1;
+
+  private moveDirection: Vector2 = new Vector2(0, 0);
+  private moveIncrement: Vector2 = new Vector2(0, 0);
   private nextPosition: Vector2 = new Vector2(0, 1);
 
-  private weaponShotTimer: number = 0;
 
+
+
+  private weaponFlippedX: boolean = false;
+  private weaponPosition: Vector2 = new Vector2(0, 0);
+  private weaponUpCorrection: number = 0;
+  private weaponZ: number = 1;
+  private weaponShotTimer: number = 0;
   private weaponAbsolutePosition: Vector2 = new Vector2();
-  private characterDirection: Vector2 = new Vector2();
+
+  private characterViewDirection: Vector2 = new Vector2();
 
   constructor(private input: Input) {
     this.setAnimation();
@@ -50,63 +56,24 @@ export class Player {
   }
 
   onUpdate(deltaTime: number): void {
-    this.moveVector.set(0, 0);
-    if (this.input.isKeyDown[Keys.Down] || this.input.isKeyDown[Keys.S]) {
-      this.currentAnimationYCoord = frameSize * 0;
-      this.moveVector.y = 1;
+    this.updateMovement(deltaTime);
 
-      this.weaponUpCorrection = 6;
-      this.weaponZ = 1;
-    } else if (this.input.isKeyDown[Keys.Up] || this.input.isKeyDown[Keys.W]) {
-      this.currentAnimationYCoord = frameSize * 3;
-      this.moveVector.y = -1;
+    this.updateFire(deltaTime);
 
-      this.weaponUpCorrection = 12;
-      this.weaponZ = -0.5;
-    }
-
-    if (this.input.isKeyDown[Keys.Left] || this.input.isKeyDown[Keys.A]) {
-      this.currentAnimationYCoord = frameSize * 1;
-      this.moveVector.x = -1;
-
-      if (!this.flippedX) {
-        this.flippedX = true;
-        this.weapon.flipVerticallyCurrentTexCoords();
-      }
-
-      this.weaponPosition.x = -32;
-    } else if (this.input.isKeyDown[Keys.Right] || this.input.isKeyDown[Keys.D]) {
-      this.currentAnimationYCoord = frameSize * 2;
-      this.moveVector.x = 1;
-
-      if (this.flippedX) {
-        this.flippedX = false;
-        this.weapon.flipVerticallyCurrentTexCoords();
-      }
-      this.weaponPosition.x = 32;
-    }
-
-    this.moveVector.normalize();
-
-    if (this.input.isKeyDown[Keys.Space] && this.weaponShotTimer <= 0) {
-      GAME_STATE.bulletManager.fire(this.weaponAbsolutePosition, this.characterDirection);
-      this.weaponShotTimer = weaponShotTimeout;
-    }
-
-    if (this.moveVector.lengthQ() > MathBase.eps) {
-      this.characterDirection.set(this.moveVector);
+    if (this.moveDirection.lengthQ() > MathBase.eps) {
+      this.characterViewDirection.set(this.moveDirection);
       this.setAnimation();
-      this.weapon.rotation = this.moveVector.toAngle() + 90;
-      this.weaponPosition.set(32 * this.moveVector.x, this.weaponUpCorrection + 32 * this.moveVector.y);
+      this.weapon.rotation = this.moveDirection.toAngle() + 90;
+      this.weaponPosition.set(32 * this.moveDirection.x, this.weaponUpCorrection + 32 * this.moveDirection.y);
       this.weapon.position.set(this.weaponPosition.x, this.weaponPosition.y, this.weaponZ);
       this.weaponAbsolutePosition.set(
         this.weaponPosition.x + this.body.position.x,
         this.weaponPosition.y + this.body.position.y);
 
       // physics
-      this.moveVector.multiplyNumSelf(this.speed * deltaTime);
+      this.moveDirection.multiplyNumSelf(this.speed * deltaTime);
       this.calculateMoveVector();
-      this.body.position.addToSelf(this.moveVector);
+      this.body.position.addToSelf(this.moveDirection);
       this.collider.center.set(this.body.position.x, this.body.position.y);
 
       // animate movement
@@ -119,6 +86,53 @@ export class Player {
     // check shot timer
     if (this.weaponShotTimer > 0) {
       this.weaponShotTimer -= deltaTime;
+    }
+  }
+
+  private updateMovement(deltaTime: number): void {
+    this.moveDirection.set(0, 0);
+    if (this.input.isKeyDown[Keys.Down] || this.input.isKeyDown[Keys.S]) {
+      this.currentAnimationYCoord = frameSize * 0;
+      this.moveDirection.y = 1;
+
+      this.weaponUpCorrection = 6;
+      this.weaponZ = 1;
+    } else if (this.input.isKeyDown[Keys.Up] || this.input.isKeyDown[Keys.W]) {
+      this.currentAnimationYCoord = frameSize * 3;
+      this.moveDirection.y = -1;
+
+      this.weaponUpCorrection = 12;
+      this.weaponZ = -0.5;
+    }
+
+    if (this.input.isKeyDown[Keys.Left] || this.input.isKeyDown[Keys.A]) {
+      this.currentAnimationYCoord = frameSize * 1;
+      this.moveDirection.x = -1;
+
+      if (!this.weaponFlippedX) {
+        this.weaponFlippedX = true;
+        this.weapon.flipVerticallyCurrentTexCoords();
+      }
+
+      this.weaponPosition.x = -32;
+    } else if (this.input.isKeyDown[Keys.Right] || this.input.isKeyDown[Keys.D]) {
+      this.currentAnimationYCoord = frameSize * 2;
+      this.moveDirection.x = 1;
+
+      if (this.weaponFlippedX) {
+        this.weaponFlippedX = false;
+        this.weapon.flipVerticallyCurrentTexCoords();
+      }
+      this.weaponPosition.x = 32;
+    }
+
+    this.moveDirection.normalize();
+  }
+
+  private updateFire(deltaTime: number) {
+    if (this.input.isKeyDown[Keys.Space] && this.weaponShotTimer <= 0) {
+      GAME_STATE.bulletManager.fire(this.weaponAbsolutePosition, this.characterViewDirection);
+      this.weaponShotTimer = weaponShotTimeout;
     }
   }
 
@@ -142,7 +156,7 @@ export class Player {
     // try both
     this.nextPosition
       .set(this.body.position)
-      .addToSelf(this.moveVector);
+      .addToSelf(this.moveDirection);
     this.nextCollider.center.set(this.nextPosition);
 
     if (!GAME_STATE.currentLevel.collide(this.nextCollider)) {
@@ -150,21 +164,21 @@ export class Player {
     }
 
     // try only x
-    this.nextPosition.set(this.body.position).addToSelf(this.moveVector.x, 0);
+    this.nextPosition.set(this.body.position).addToSelf(this.moveDirection.x, 0);
     this.nextCollider.center.set(this.nextPosition);
     if (!GAME_STATE.currentLevel.collide(this.nextCollider)) {
-      this.moveVector.y = 0;
+      this.moveDirection.y = 0;
       return;
     }
 
     // try only y
-    this.nextPosition.set(this.body.position).addToSelf(0, this.moveVector.y);
+    this.nextPosition.set(this.body.position).addToSelf(0, this.moveDirection.y);
     this.nextCollider.center.set(this.nextPosition);
     if (!GAME_STATE.currentLevel.collide(this.nextCollider)) {
-      this.moveVector.x = 0;
+      this.moveDirection.x = 0;
       return;
     }
 
-    this.moveVector.set(0, 0);
+    this.moveDirection.set(0, 0);
   }
 }
