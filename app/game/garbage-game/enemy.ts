e0abf298faecc24bec2b10f2aedd4ef2d20a5686
perfectdaemon <perfectdaemon@gmail.com>
2018-02-25 00:23:20 +0700

    enemy changes color for a bit when being hit

--- app/game/garbage-game/enemy.ts
+++ app/game/garbage-game/enemy.ts
@@ -4,10 +4,13 @@ import { Sprite } from '../../engine/scene/sprite';
 import { GAME_STATE } from './game-state';
 import { AABB } from './physics/aabb';
 import { IPoolItem } from './pool/ipool-item';
+import { Vector4 } from '../../engine/math/vector4';
+import { isEqual } from '../../engine/math/math-base';
 
 const defaultEnemySpeed = 50;
 const defaultEnemyMaxHealth = 2;
 const colliderReduceSize = 30;
+const defaultHitTimer = 0.1;
 
 export class Enemy implements IPoolItem {
   active: boolean = false;
@@ -24,6 +27,9 @@ export class Enemy implements IPoolItem {
   private moveIncrement: Vector2 = new Vector2();
   private flippedX: boolean = false;
 
+  private verticesColor: Vector4 = new Vector4(1, 1, 1, 1);
+  private hitTimer: number = 0;
+
   constructor(textureRegion: TextureRegion, multSize: number = 1) {
     this.body = new Sprite();
     this.body.position.z = 1;
@@ -38,6 +44,8 @@ export class Enemy implements IPoolItem {
   onActivate(): void {
     this.body.visible = true;
     this.health = this.maxHealth;
+    this.hitTimer = 0;
+    this.verticesColor.set(1, 1, 1, 1);
   }
 
   onDeactivate(): void {
@@ -61,10 +69,24 @@ export class Enemy implements IPoolItem {
       this.flippedX = false;
       this.body.flipVerticallyCurrentTexCoords();
     }
+
+    this.body.setVerticesColor(this.verticesColor);
+
+    if (this.hitTimer > 0) {
+      this.hitTimer -= deltaTime;
+
+      if (this.hitTimer <= 0) {
+        this.verticesColor.set(1, 1, 1, 1);
+      }
+    }
   }
 
   hit(damage: number): void {
     this.health -= damage;
+
+    this.hitTimer = defaultHitTimer;
+    this.verticesColor.set(1, 0.5, 0.5, 0.5);
+
 
     if (this.health <= 0) {
       this.active = false;
