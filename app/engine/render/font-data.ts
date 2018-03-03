bcb9d4567dda8b00f40de9b3c62d47ddcc5e8951
perfectdaemon <perfectdaemon@gmail.com>
2018-03-03 23:10:58 +0700

    FontData finished

--- app/engine/render/font-data.ts
+++ app/engine/render/font-data.ts
@@ -1,3 +1,11 @@
+// tslint:disable-next-line:interface-name
 export interface FontData {
   symbol: string;
+  startY: number;
+  width: number;
+  height: number;
+  tx: number;
+  ty: number;
+  tw: number;
+  th: number;
 }
