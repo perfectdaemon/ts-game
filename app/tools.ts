6583aa92a9e260a05421129d5cedf470f11f2e86
perfectdaemon <perfectdaemon@gmail.com>
2018-03-10 12:22:48 +0700

    added first implementation of font generator

--- app/tools.ts
+++ app/tools.ts
@@ -1,5 +1,14 @@
+import { SymbolData } from './engine/render/symbol-data';
+
 export class BitmapFontGenerator {
   private context: CanvasRenderingContext2D;
+  private text: string = `
+ 0123456789!@#$%^&*()_+|{}:"<>?-=\[];',./~\`«–»—
+abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
+абвгдеёжзийклмнопрстуфхцчшщъыьэюяАБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ
+`;
+
+  private symbolDatas: SymbolData[] = [];
 
   constructor(private canvasElement: HTMLCanvasElement, private fontDropElement: HTMLElement) {
     fontDropElement.ondrop = (event) => {
@@ -9,9 +18,50 @@ export class BitmapFontGenerator {
     };
 
     this.context = canvasElement.getContext('2d') as CanvasRenderingContext2D;
+    this.context.fillStyle = '#333';
+    this.context.fillRect(0, 0, canvasElement.width, canvasElement.height);
+  }
+
+  draw(
+    fontSize: number, fontName: string,
+    textureWidth: number, textureHeight: number,
+    paddingTop: number, paddingRight: number,
+  ): void {
+    this.context.fillStyle = '#000';
+    this.context.fillRect(0, 0, textureWidth, textureHeight);
+
+    this.context.fillStyle = '#fff';
+    this.context.font = `${fontSize}px ${fontName}`;
+
+    let startX = 0;
+    let startY = fontSize;
+    for (const letter of this.text) {
+      const symbolWidth = this.context.measureText(letter).width;
+      if (symbolWidth === 0 || letter === '\n') { continue; }
+      if (symbolWidth + startX > textureWidth) {
+        startY += fontSize + paddingTop;
+        startX = 0;
+      }
+      const symbolData = new SymbolData();
+      symbolData.width = symbolWidth;
+      symbolData.startY = 0;
+      symbolData.height = fontSize;
+      symbolData.symbol = letter;
+      symbolData.tx = startX / textureWidth;
+      symbolData.ty = startY / textureHeight;
+      symbolData.tw = symbolWidth / textureWidth;
+      symbolData.th = fontSize / textureHeight;
+      this.symbolDatas.push(symbolData);
+
+      this.context.fillText(letter, startX, startY);
+      startX += symbolWidth + paddingRight;
+    }
+
+    console.log(this.symbolDatas);
   }
 }
 
 const canvas = document.getElementById('canvas-main') as HTMLCanvasElement;
 const fontDropEl = document.getElementById('fontDrop') as HTMLElement;
 const bfg = new BitmapFontGenerator(canvas, fontDropEl);
+bfg.draw(30, 'Arial', 512, 256, 0, 1);
