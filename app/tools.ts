0a15a62ce611e4314fe1f65ff125328302751722
perfectdaemon <perfectdaemon@gmail.com>
2018-03-10 17:24:15 +0700

    bettet bitmap font generation (result is good, but code is smell)

--- app/tools.ts
+++ app/tools.ts
@@ -2,6 +2,9 @@ import { SymbolData } from './engine/render/symbol-data';
 
 export class BitmapFontGenerator {
   private context: CanvasRenderingContext2D;
+
+  private letterCanvas: HTMLCanvasElement;
+  private letterContext: CanvasRenderingContext2D;
   private text: string = `
  0123456789!@#$%^&*()_+|{}:"<>?-=\[];',./~\`«–»—
 abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
@@ -18,6 +21,48 @@ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
     };
 
     this.context = canvasElement.getContext('2d') as CanvasRenderingContext2D;
+
+    this.letterCanvas = document.createElement('canvas');
+    this.letterCanvas.width = 256;
+    this.letterCanvas.height = 256;
+    this.letterContext = this.letterCanvas.getContext('2d') as CanvasRenderingContext2D;
+  }
+
+  drawLetterToHiddenCanvas(letter: string, fontSize: number, fontName: string): number[] {
+    this.letterContext.clearRect(0, 0, this.letterCanvas.width, this.letterCanvas.height);
+    this.letterContext.fillStyle = '#fff';
+    this.letterContext.font = `${fontSize}px ${fontName}`;
+    const symbolRectWidth = this.letterContext.measureText(letter).width + 5;
+    const symbolRectHeight = fontSize * 2;
+    this.letterContext.fillText(letter, 5, fontSize + 5);
+
+    const imageData = this.letterContext.getImageData(0, 0, symbolRectWidth, symbolRectHeight);
+
+    return [this.scanRectForTop(imageData) - 5, this.scanRectForBottom(imageData) - 5];
+  }
+
+  scanRectForTop(imageData: ImageData): number {
+    for (let row = 0; row < imageData.height; ++row) {
+      for (let column = 0; column < imageData.width; ++column) {
+        const pixel = row * imageData.width * 4 + column * 4;
+        if (imageData.data[pixel + 3] > 0) {
+          return row;
+        }
+      }
+    }
+    return 0;
+  }
+
+  scanRectForBottom(imageData: ImageData): number {
+    for (let row = imageData.height; row >= 0; --row) {
+      for (let column = 0; column < imageData.width; ++column) {
+        const pixel = row * imageData.width * 4 + column * 4;
+        if (imageData.data[pixel + 3] > 0) {
+          return row;
+        }
+      }
+    }
+    return 0;
   }
 
   draw(
@@ -37,22 +82,21 @@ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
         startY += fontSize + paddingTop;
         startX = 0;
       }
+      const tmpTopStartY = this.drawLetterToHiddenCanvas(letter, fontSize, fontName);
       const symbolData = new SymbolData();
       symbolData.width = symbolWidth;
-      symbolData.height = fontSize;
+      symbolData.height = tmpTopStartY[1] - tmpTopStartY[0] + 1;
       symbolData.symbol = letter;
+      symbolData.startY = tmpTopStartY[0];
       symbolData.tx = startX / textureWidth;
-      symbolData.ty = startY / textureHeight;
-      symbolData.tw = symbolWidth / textureWidth;
-      symbolData.th = fontSize / textureHeight;
+      symbolData.ty = (startY - fontSize + tmpTopStartY[0]) / textureHeight;
+      symbolData.tw = symbolData.width / textureWidth;
+      symbolData.th = symbolData.height / textureHeight;
       this.symbolDatas.push(symbolData);
 
       this.context.fillText(letter, startX, startY);
       startX += symbolWidth + paddingRight;
     }
-
-    console.log(this.symbolDatas);
-    console.log(this.canvasElement.toDataURL('image/png'));
   }
 
   save(width: number, height: number): void {
@@ -77,5 +121,5 @@ abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ
 const canvas = document.getElementById('canvas-main') as HTMLCanvasElement;
 const fontDropEl = document.getElementById('fontDrop') as HTMLElement;
 const bfg = new BitmapFontGenerator(canvas, fontDropEl);
-bfg.draw(30, 'Arial', 512, 256, 0, 1);
+bfg.draw(30, 'Arial', 512, 256, 1, 1);
 bfg.save(512, 256);
