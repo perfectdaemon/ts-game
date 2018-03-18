755c1b324c919a0172ebc203cabfab33d4373f8b
perfectdaemon <perfectdaemon@gmail.com>
2018-03-18 22:54:47 +0700

    rts: added default font and resources

--- app/game/rts-game/assets/assets.ts
+++ app/game/rts-game/assets/assets.ts
@@ -1,7 +1,29 @@
+import { LoaderFactory } from '../../../engine/loaders/loader-factory';
+import { Font } from '../../../engine/render/font';
+import { Material } from '../../../engine/render/material';
+import { ShaderProgram } from '../../../engine/render/shader-program';
+import { DEFAULT_FONT, DEFAULT_MATERIAL, DEFAULT_SHADER } from './default';
+
 export class Assets {
+  shader: ShaderProgram = new ShaderProgram();
+  blankMaterial: Material = new Material();
+  font: Font = new Font();
+
+  private loaders: LoaderFactory = new LoaderFactory();
+
   loadAll(): Promise<void> {
     return new Promise<void>((resolve, reject) => {
-      resolve();
+      Promise.all<ShaderProgram, Font>([
+        this.loaders.loadShaderProgram(DEFAULT_SHADER),
+        this.loaders.loadFont(DEFAULT_FONT)
+      ])
+        .then(result => {
+          [this.shader, this.font] = result;
+          DEFAULT_MATERIAL.shaderProgram = this.shader;
+        })
+        .then(() => this.loaders.loadMaterial(DEFAULT_MATERIAL))
+        .then(material => this.blankMaterial = material)
+        .then(() => resolve());
     });
   }
 }
