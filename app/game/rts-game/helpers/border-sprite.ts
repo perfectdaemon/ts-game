ae5beabd561ebcb747618902f95bd09ad64288fe
perfectdaemon <perfectdaemon@gmail.com>
2018-03-20 22:46:43 +0700

    BorderSprite - added visible, parent and color properties

--- app/game/rts-game/helpers/border-sprite.ts
+++ app/game/rts-game/helpers/border-sprite.ts
@@ -1,8 +1,15 @@
 import { Vector2 } from '../../../engine/math/vector2';
+import { Vector4 } from '../../../engine/math/vector4';
+import { Node } from '../../../engine/scene/node';
 import { Sprite } from '../../../engine/scene/sprite';
 
 export class BorderSprite {
+
   sprites = new Array<Sprite>(4);
+  color: Vector4 = new Vector4(1, 1, 1, 1);
+
+  private _visible: boolean = true;
+  private _parent: Node | null = null;
 
   constructor(
     public start: Vector2,
@@ -11,6 +18,18 @@ export class BorderSprite {
   ) {
     this.createSprites();
     this.updateSprites();
+  }
+
+  get visible() { return this._visible; }
+  set visible(value: boolean) {
+    this._visible =
+      this.sprites[0].visible = this.sprites[1].visible = this.sprites[2].visible = this.sprites[3].visible = value;
+  }
+
+  get parent() { return this._parent; }
+  set parent(value: Node | null) {
+    this._parent =
+      this.sprites[0].parent = this.sprites[1].parent = this.sprites[2].parent = this.sprites[3].parent = value;
   }
 
   updateSprites(): void {
@@ -25,13 +44,17 @@ export class BorderSprite {
 
     this.sprites[3].setSize(this.borderWidth, this.finish.y - this.start.y);
     this.sprites[3].position.set(this.start);
+
+    for (const sprite of this.sprites) {
+      sprite.setVerticesColor(this.color);
+    }
   }
 
   private createSprites(): void {
     this.sprites[0] = new Sprite(this.finish.x - this.start.x, this.borderWidth, new Vector2(0, 0)); // top
     this.sprites[1] = new Sprite(this.borderWidth, this.finish.y - this.start.y, new Vector2(1, 0)); // right
     this.sprites[2] = new Sprite(this.finish.x - this.start.x, this.borderWidth, new Vector2(0, 1)); // bottom
-    this.sprites[3] = new Sprite(this.borderWidth, this.finish.y - this.start.y, new Vector2(1, 0)); // left
+    this.sprites[3] = new Sprite(this.borderWidth, this.finish.y - this.start.y, new Vector2(0, 0)); // left
 
     for (const sprite of this.sprites) {
       sprite.position.z = 1;
