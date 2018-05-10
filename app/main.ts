e239030b300934a1aa5c61eb0ee510b4e2e3d14a
perfectdaemon <perfectdaemon@gmail.com>
2018-05-10 23:13:00 +0700

    #18 Added template project

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,4 @@
-import { Game } from './game/rts-game/game';
+import { Game } from './game/template-game/game';
 
 const canvas = document.getElementById('canvas-main') as HTMLCanvasElement;
 if (canvas) {
