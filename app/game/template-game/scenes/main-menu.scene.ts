0a09185b2f0dddf97bb4307f2d885b71ceb64efc
perfectdaemon <perfectdaemon@gmail.com>
2018-05-12 23:15:25 +0700

    template game - MainMenu scene now has GuiManager

--- app/game/template-game/scenes/main-menu.scene.ts
+++ app/game/template-game/scenes/main-menu.scene.ts
@@ -1,22 +1,42 @@
+import { GuiManager } from '../../../engine/gui/gui-manager';
 import { Keys, MouseButtons } from '../../../engine/input/keys.enum';
 import { Vector2 } from '../../../engine/math/vector2';
+import { SpriteBatch } from '../../../engine/render2d/sprite-batch';
+import { TextBatch } from '../../../engine/render2d/text-batch';
 import { Scene } from '../../../engine/scenes/scene';
 import { GLOBAL } from '../global';
 
 export class MainMenuScene extends Scene {
+  guiManager: GuiManager;
+
   constructor() {
     super();
   }
 
   load(): Promise<void> {
+    this.guiManager = new GuiManager(
+      GLOBAL.assets.blankMaterial,
+      new SpriteBatch(),
+      new TextBatch(GLOBAL.assets.font),
+      GLOBAL.assets.guiCamera,
+    );
+
     return super.load();
+  }
+
+  unload(): Promise<void> {
+    this.guiManager.free();
+
+    return super.unload();
   }
 
   render(): void {
     GLOBAL.assets.guiCamera.update();
+    this.guiManager.render();
   }
 
   update(deltaTime: number): void {
+    this.guiManager.update(deltaTime);
   }
 
   onKeyDown(key: Keys): void {
