dc5c02d6cc3e578e4f1bdb0e5e1f74ab60c5bed6
perfectdaemon <perfectdaemon@gmail.com>
2018-08-11 00:33:48 +0700

    Huge npm packages update

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,6 +2,6 @@ const merge = require('webpack-merge');
 const common = require('./webpack.config.js');
 
 module.exports = merge(common, {
-  // mode: 'development',
+  mode: 'development',
   devtool: 'inline-source-map'
 });
