2252fc6146efc35c8cd2d521cecd7bb59c576e2a
perfectdaemon <perfectdaemon@gmail.com>
2019-01-04 16:39:22 +0700

    IGDC-159 Added DialogBox to scene and implemented some state machine for turn based battle

--- app/game/rpg-game/scenes/fight.scene.ts
+++ app/game/rpg-game/scenes/fight.scene.ts
@@ -1,14 +1,28 @@
 import { Keys, MouseButtons } from '../../../engine/input/keys.enum';
 import { Vector2 } from '../../../engine/math/vector2';
 import { Scene } from '../../../engine/scenes/scene';
+import { DialogBox } from '../dialog-box';
 import { FIGHT_GAME_STATE } from '../fight/game-state';
 import { Player } from '../fight/player';
 import { GLOBAL } from '../global';
 import { RenderHelper } from '../render-helper';
 
+export enum FightState {
+  Start,
+  HumanTurnProtect,
+  HumanTurnAttack,
+  AiTurn,
+  Animation,
+  Victory,
+  Defeat,
+}
+
 export class FightScene extends Scene {
   human: Player;
   enemy: Player;
+  fightState: FightState;
+
+  dialog: DialogBox;
 
   renderHelper: RenderHelper;
 
@@ -19,13 +33,14 @@ export class FightScene extends Scene {
   load(): Promise<void> {
     console.log('Fight scene is loaded');
     this.renderHelper = new RenderHelper(GLOBAL.assets.font, GLOBAL.assets.blankMaterial);
+    this.dialog = new DialogBox();
     this.reset();
     return super.load();
   }
 
   render(): void {
     GLOBAL.assets.gameCamera.update();
-    this.renderHelper.render([this.human, this.enemy]);
+    this.renderHelper.render([this.human, this.enemy, this.dialog]);
   }
 
   update(deltaTime: number): void {
@@ -46,5 +61,40 @@ export class FightScene extends Scene {
   private reset(): void {
     this.human = Player.build(FIGHT_GAME_STATE.humanData);
     this.enemy = Player.build(FIGHT_GAME_STATE.enemyData);
+    this.setFightState(FightState.Start);
+  }
+
+  private setFightState(newState: FightState): void {
+    switch (newState) {
+      case FightState.Start:
+        this.dialog.text.text = 'Бой скоро начнется, приготовьтесь';
+        break;
+
+      case FightState.HumanTurnProtect:
+        this.dialog.text.text = `Выберите ${this.human.protectsLeft} своих отсеков для защиты`;
+        break;
+
+      case FightState.HumanTurnAttack:
+        this.dialog.text.text = `Выберите ${this.human.protectsLeft} отсеков противника для атаки`;
+        break;
+
+      case FightState.AiTurn:
+        this.dialog.text.text = `Ход противника`;
+        break;
+
+      case FightState.Animation:
+        this.dialog.text.text = `...`;
+        break;
+
+      case FightState.Victory:
+        this.dialog.text.text = `Вы победили!`;
+        break;
+
+      case FightState.Defeat:
+        this.dialog.text.text = `Вы проиграли!`;
+        break;
+    }
+
+    this.fightState = newState;
   }
 }
