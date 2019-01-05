0dd0ab4e87ac7349c1613d7484813555c40998da
perfectdaemon <perfectdaemon@gmail.com>
2019-01-05 14:34:03 +0700

    IGDC-159 Fixed `undefined` in name, added update health

--- app/game/rpg-game/fight/heal-item.ts
+++ app/game/rpg-game/fight/heal-item.ts
@@ -2,7 +2,7 @@ import { Item } from './item';
 import { Player } from './player';
 
 export class HealItem extends Item {
-  name: string = `Вылечить ${this._amount} единиц здоровья`;
+  get name(): string { return `Вылечить ${this._amount} единиц здоровья`; }
 
   removeAfterNumberOfTurns: number = 0;
 
@@ -14,6 +14,7 @@ export class HealItem extends Item {
 
   internalUse(self: Player, other: Player): void {
     self.ship.health = Math.min(self.ship.health + this._amount, self.playerData.shipHealth);
+    self.ship.renderable.updateHealth(self.ship.health);
   }
 
   removeEffect(self: Player, other: Player): void { }
