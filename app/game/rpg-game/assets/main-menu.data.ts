46abc6b598551dea4444e134096470f1cf3ed7bf
perfectdaemon <perfectdaemon@gmail.com>
2019-01-14 23:58:12 +0700

    IGDC-159 Deleted test buttons from main menu

--- app/game/rpg-game/assets/main-menu.data.ts
+++ app/game/rpg-game/assets/main-menu.data.ts
@@ -5,7 +5,6 @@ import { MenuData } from '../menu/menu-data';
 
 const SCREEN_CENTER_X = 480;
 const BUTTON_START_Y = 400;
-const BUTTON_MARGIN_Y = 60;
 
 export const MAIN_MENU_DATA: MenuData = {
   background: {},
@@ -19,42 +18,6 @@ export const MAIN_MENU_DATA: MenuData = {
       position: new Vector3(SCREEN_CENTER_X, BUTTON_START_Y, 1),
       pivotPoint: new Vector2(0.5, 0.5),
       verticesColor: new Vector4(0.9, 0.8, 0.8, 1.0),
-      hoverVerticesColor: new Vector4(1.0, 1.0, 1.0, 1.0),
-    },
-
-    {
-      name: 'TreasureButton',
-      labelText: 'Test Treasure',
-      labelColor: new Vector4(0.3, 0.3, 0.3, 1.0),
-      labelScale: 1.0,
-      size: new Vector2(150, 40),
-      position: new Vector3(SCREEN_CENTER_X, BUTTON_START_Y + BUTTON_MARGIN_Y, 1),
-      pivotPoint: new Vector2(0.5, 0.5),
-      verticesColor: new Vector4(0.8, 0.9, 0.8, 1.0),
-      hoverVerticesColor: new Vector4(1.0, 1.0, 1.0, 1.0),
-    },
-
-    {
-      name: 'TestFightButton',
-      labelText: 'Test Fight',
-      labelColor: new Vector4(0.3, 0.3, 0.3, 1.0),
-      labelScale: 1.0,
-      size: new Vector2(150, 40),
-      position: new Vector3(SCREEN_CENTER_X, BUTTON_START_Y + BUTTON_MARGIN_Y * 2, 1),
-      pivotPoint: new Vector2(0.5, 0.5),
-      verticesColor: new Vector4(0.8, 0.8, 0.9, 1.0),
-      hoverVerticesColor: new Vector4(1.0, 1.0, 1.0, 1.0),
-    },
-
-    {
-      name: 'TestPlanetButton',
-      labelText: 'Test Planet',
-      labelColor: new Vector4(0.3, 0.3, 0.3, 1.0),
-      labelScale: 1.0,
-      size: new Vector2(150, 40),
-      position: new Vector3(SCREEN_CENTER_X, BUTTON_START_Y + BUTTON_MARGIN_Y * 3, 1),
-      pivotPoint: new Vector2(0.5, 0.5),
-      verticesColor: new Vector4(0.8, 0.9, 0.9, 1.0),
       hoverVerticesColor: new Vector4(1.0, 1.0, 1.0, 1.0),
     },
   ],
