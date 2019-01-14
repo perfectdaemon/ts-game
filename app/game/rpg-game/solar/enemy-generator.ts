2d32876bba65e32627e0afca54a620c5681dae03
Dmitry Orlov <perfectdaemon@gmail.com>
2019-01-14 16:35:35 +0700

    IGDC-159 Item generation for ship

--- app/game/rpg-game/solar/enemy-generator.ts
+++ app/game/rpg-game/solar/enemy-generator.ts
@@ -1,9 +1,10 @@
 import { Vector2 } from '../../../engine/math/vector2';
-import { PlayerData, ShipCellData } from '../player-data';
+import { ItemGenerator } from '../item-generator';
+import { ItemType, PlayerData, ShipCellData } from '../player-data';
 
 export class EnemyGenerator {
   static generate(power: number): PlayerData {
-    const cellVariant: ShipCellData[] = CELL_VARIANTS[1].cells
+    const cellVariant: ShipCellData[] = CELL_VARIANTS[Math.floor(CELL_VARIANTS.length * Math.random())].cells
       .map(it => ({ position: it }));
 
     const data: PlayerData = {
@@ -16,6 +17,18 @@ export class EnemyGenerator {
       inventorySize: 0,
       inventory: [],
     };
+
+    let maxWeapons = power;
+    let maxShields = power;
+    for (const cell of data.cells) {
+      if (maxWeapons > 0) {
+        maxWeapons -= 0.15;
+        cell.item = ItemGenerator.generate(ItemType.Weapon, power);
+      } else if (maxShields > 0) {
+        maxShields -= 0.15;
+        cell.item = ItemGenerator.generate(ItemType.Shield, power);
+      }
+    }
 
     return data;
   }
